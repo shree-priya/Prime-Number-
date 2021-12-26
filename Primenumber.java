1422750e494983ef8aa3fa03adc1653463949dce
shree-priya <96342215+shree-priya@users.noreply.github.com>
2021-12-26 10:00:56 +0530

    Update Primenumber.java

--- Primenumber.java
+++ Primenumber.java
@@ -13,12 +13,9 @@ static void checkPrime(int n){
     }      
    }      
    if(flag==0)  { System.out.println(n+" is prime number"); }  
-  }//end of else  
+  }  
 }  
- public static void main(String args[]){    
-  checkPrime(1);  
+ public static void main(String args[]){      
   checkPrime(3);  
-  checkPrime(17);  
-  checkPrime(20);  
 }    
 }
